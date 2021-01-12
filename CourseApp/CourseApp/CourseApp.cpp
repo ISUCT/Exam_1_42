ef1e965cc8e54d4745f106103c9062be0817edab
purpleebaniy <72213222+purpleebaniy@users.noreply.github.com>
2021-01-12 13:32:48 +0300

    KarneevMisha

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,35 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
+using namespace std;
 
-int main()
-{
-		std::cout << "Exam" << std::endl;
+
+
+void resh(int* arr, int size) {
+	int n = 1;
+	int a;
+	cout << arr[0];
+	for (int i = 0; i < size - 1; i++) {
+
+		if (arr[i] == arr[i + 1]) {
+			cout << 0;
+		}
+		else
+			cout << 1;
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void main() {
+	int size = 5;
+	cin >> size;
+	int* arr = new int[size];
+
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+	}
+	int*& arrRef = arr;
+	resh(arrRef, size);
+
+	delete[] arr;
+
+
+}
