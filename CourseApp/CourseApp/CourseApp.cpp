191f3a9a370c34784890f50f12591902a5e159e2
Alexander Seleznev <fizika10a2@yandex.ru>
2021-01-12 13:15:17 +0300

    Exam

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,34 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
+using namespace std;
 
+class maximum {
+public:
+	void max() {
+		int maximum = -2147483648;
+		int n;
+		int i;
+		cout << "¬ведите длину массива :"; cin >> n;
+		int* arr = new int[n];
+		for (i = 1; i < n + 1; i++) {
+			cin >> *arr;
+			if (*arr > maximum) {
+				maximum = *arr;
+			}
+
+		}
+
+		cout << maximum << endl;
+	}
+
+};
 int main()
 {
-		std::cout << "Exam" << std::endl;
+	setlocale(LC_ALL, "Russian");
+
+	maximum a;
+
+	a.max();
+
+	return 0;
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
