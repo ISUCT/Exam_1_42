2405e8504c0b957ba7aced08d71eab63013939ef
Глеб Бялек <glebbyalek@gmail.com>
2021-01-12 17:46:33 +0300

    Bylek Gleb Exam

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,32 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
 
+using namespace std;
+
+class MaxOfArray
+{
+public:
+    static double FindMax(double Array[], int length)
+    {
+        double Max = 0;
+        for (int i = 0; i < length; i++)
+        {
+            if (i == 1)  Max = Array[i];
+            if (Array[i] > Max)
+                Max = Array[i];
+        }
+        return Max;
+    }
+};
 int main()
 {
-		std::cout << "Exam" << std::endl;
+    const int length = 5;
+    double Array[length];
+    cout << "Enter 5 elements" << endl;
+    for (int i = 0; i < length; i++)
+    {
+        cin >> Array[i];
+    }
+    double max = MaxOfArray::FindMax(Array, length);
+    cout << MaxOfArray::FindMax(Array, length) << endl;
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
