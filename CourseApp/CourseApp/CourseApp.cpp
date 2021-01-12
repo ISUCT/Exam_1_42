265b7d3d202bf23d374c20d1b5eaf2931463dafe
progonskaij <72213216+progonskaij@users.noreply.github.com>
2021-01-12 14:20:25 +0300

    Exam_1_42

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,35 @@
 // CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
+#include<iostream>
+#include<string>
 
-#include <iostream>
-#include <cmath>
+using namespace std;
+class TemperatureConverter
+{
 
+public:
+	string convertTemp(int temperature, char convertTo)
+	{
+
+		string r = "nothing";
+		if (convertTo == 'C' || convertTo == 'c')
+		{
+			r = "Цельсий = " + to_string(((float)temperature - 32) * 5 / 9);
+		}
+		else
+		{
+
+			(convertTo == 'F' || convertTo == 'f') r = "фаренгейт = " + to_string(9 * (float)temperature / 5 + 32);
+		}
+
+		return r;
+	}
+};
 int main()
 {
-		std::cout << "Exam" << std::endl;
+	TemperatureConverter type;
+	cout << type.convertTemp(8, 'f') << endl;
+	cout << type.convertTemp(2, 'c') << endl;
+	cout << type.convertTemp(2, 'k') << endl;
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
