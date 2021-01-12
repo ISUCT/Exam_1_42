569ab993cd19d89779315d8e060be0be769ebc0f
Дмитрий Малахов <dmitriy_jg@mail.ru>
2021-01-12 12:28:39 +0300

    Malachov

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,46 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
+#include <string>
+
+using namespace std;
+
+class Worker
+
+{
+public:
+    string surname;
+    int experience;
+    int payment;
+    int hours;
+
+    Worker(string s, int a, int b, int c) //конструктор    
+    {
+        surname = s;
+        experience = a;
+        payment = b;
+        hours = c;
+    }
+
+    void display()
+    {
+        cout << "Surname: " << surname << endl;
+        cout << "Experience: " << experience << " years" << endl;
+        cout << "Hourly payment: " << payment << "$" << endl;
+        cout << "Number of hours worked: " << hours  << endl;
+        cout << "-----------------------" << endl;
+    }
+    ~Worker() {}; //деструктор 
+};
+
 
 int main()
 {
-		std::cout << "Exam" << std::endl;
+    Worker first ("Malakhov", 3, 1000, 6);
+    Worker second ("Morgenstern", 4, 15, 8);
+    Worker third("Smirnov", 11, 500, 7);
+ 
+    first.display();
+    second.display();
+    third.display();
+  
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
