785541d2ab275e08c4f2b7f7f22374e6e662f1fc
markru <mrk.ru@list.ru>
2021-01-12 12:25:27 +0300

    Exam_Mark

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,33 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
+#include <string>
 
-int main()
+
+void line(std::string str, int len)
 {
-		std::cout << "Exam" << std::endl;
+    std::cout << '0';    //the first number will always be 0, because compare with the previous one
+
+    for (int i = 1; i < len; i++)
+    {
+
+        if (str[i] == str[i - 1])
+            {
+                std::cout << '0';
+            }
+        else
+            {
+                std::cout << '1';
+            }
+    }
+} 
+int main()
+
+{
+    std::string str;
+    std::cin >> str;
+
+    int len = str.size() / sizeof(char);   //determining string length by string method
+
+    line(str, len);   //method
+
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
